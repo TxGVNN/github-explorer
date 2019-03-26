e54ca70bdb98a260af7384f38b9962e2ed1ab17a
Giap Tran <txgvnn@gmail.com>
2019-03-26 22:07:58 +0700

    POC 2 - render the trees

--- github-mode.el
+++ github-mode.el
@@ -9,6 +9,7 @@
 (setq request-message-level 'trace)
 (setq request-curl-options '("-k"))
 
+(defvar github-buffer-temp)
 ;; * TODO will also accept a full link https://github...
 ;; * TODO entering to github-mode
 (defun github-go (&optional repo)
@@ -23,17 +24,23 @@
 This function will create *GITHUB:REPO:* buffer"
   (let (url)
     (setq url (format "https://api.github.com/repos/%s/git/trees/%s" repo tree))
-    (setq buffer (format "*GITHUB:%s:*" repo))
+    (setq github-buffer-temp (format "*Github:%s:*" repo))
     (request
      url
      :parser 'buffer-string
      :success
      (cl-function (lambda (&key data &allow-other-keys)
                     (when data
-                      (with-current-buffer (get-buffer-create buffer)
-                        (erase-buffer)
-                        (insert data)
-                        (pop-to-buffer (current-buffer))))))
+                      (with-current-buffer (get-buffer-create github-buffer-temp)
+                        (let (gh-object)
+                          (erase-buffer)
+                          (insert data)
+                          (pop-to-buffer (current-buffer))
+                          (beginning-of-buffer)
+                          (setq gh-object (json-read))
+                          (erase-buffer)
+                          (github--render-object gh-object))
+                        ))))
      :error
      (cl-function (lambda (&key error-thrown &allow-other-keys&rest _)
                     (message "Got error: %S" error-thrown)))
@@ -42,7 +49,7 @@ This function will create *GITHUB:REPO:* buffer"
 (defun github-raw(&optional path)
   "Get raw of PATH in *GITHUB:REPO* buffer.
 This function is have to called in *GITHUB:REPO* buffer"
-  (interactive (list (thing-at-point 'symbol)))
+  (interactive (list (thing-at-point 'filename)))
   (setq path (read-string "Path: " path))
   (let ((pos 0) matches repo)
     (setq repo (buffer-name))
@@ -56,17 +63,17 @@ This function is have to called in *GITHUB:REPO* buffer"
 ;; * QUESTION support revision? only master?
 (defun github--raw (&optional repo path)
   "Get raw of PATH in REPO github."
-  (let (url buffer)
+  (let (url)
     (setq url (format "https://raw.githubusercontent.com/%s/master/%s" repo path))
-    (setq buffer (format "*GITHUB:%s:%s*" repo path))
-    (message "%s" buffer)
+    (setq github-buffer-temp (format "*Github:%s:%s*" repo path))
     (request
      url
+     ;; "https://api.github.com/repos/txgvnn/dots/git/trees/master"
      :parser 'buffer-string
      :success
      (cl-function (lambda (&key data &allow-other-keys)
                     (when data
-                      (with-current-buffer (get-buffer-create buffer)
+                      (with-current-buffer (get-buffer-create github-buffer-temp)
                         (erase-buffer)
                         (insert data)
                         (pop-to-buffer (current-buffer))))))
@@ -74,7 +81,22 @@ This function is have to called in *GITHUB:REPO* buffer"
      (cl-function (lambda (&key error-thrown &allow-other-keys&rest _)
                     (message "Got error: %S" error-thrown)))
      :complete (lambda (&rest _) (message "Finished!")))))
-
 ;; (github--raw "melpa/melpa" "README.md")
+
+
+(defun github--render-object (gh-object)
+  "Render the GH-OBJECT."
+  (let (item i trees)
+    (setq trees (cdr (assoc 'tree gh-object)))
+    (setq i 0)
+    (while (< i (length trees))
+      (setq item (elt trees i))
+      (if (string= (cdr (assoc 'type item)) "tree")
+          (insert (format "[+] %s\n" (cdr (assoc 'path item))))
+        (insert (format "--- %s\n" (cdr (assoc 'path item)))))
+      (setq i (+ i 1))
+      )
+    )
+  )
 
 (provide 'github)
