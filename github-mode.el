38b93a675c206ec0aa76f7ffca3e782ed5b2f435
Giap Tran <txgvnn@gmail.com>
2019-04-08 12:53:23 +0700

    Add license information

--- github-mode.el
+++ github-mode.el
@@ -6,6 +6,22 @@
 ;; URL: https://github.com/TxGVNN/github-mode
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4") (request "0.1.0"))
+;; Keywords: Github
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; M-x github-go "txgvnn/github-mode"
