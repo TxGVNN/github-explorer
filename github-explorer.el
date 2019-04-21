3a67835b24366c64deb2aa33626330742895bb0c
Giap Tran <txgvnn@gmail.com>
2019-04-21 12:10:44 +0700

    Rename github-mode -> github-explorer

--- github-explorer.el
+++ github-explorer.el
@@ -1,9 +1,9 @@
-;;; github-mode.el --- Explore a GitHub repository on the fly -*- lexical-binding: t -*-
+;;; github-explorer.el --- Explore a GitHub repository on the fly -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2019 Giap Tran <txgvnn@gmail.com>
 
 ;; Author: Giap Tran <txgvnn@gmail.com>
-;; URL: https://github.com/TxGVNN/github-mode
+;; URL: https://github.com/TxGVNN/github-explorer
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4") (request "0.1.0"))
 ;; Keywords: comm
@@ -24,7 +24,7 @@
 ;; see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; M-x github-go "txgvnn/github-mode"
+;; M-x github-explorer "txgvnn/github-explorer"
 
 ;;; Code:
 
@@ -35,43 +35,43 @@
 (defgroup github nil
   "Major mode of GitHub configuration file."
   :group 'languages
-  :prefix "github-")
+  :prefix "github-explorer-")
 
-(defcustom github-mode-hook nil
-  "*Hook run by `github-mode'."
+(defcustom github-explorer-hook nil
+  "*Hook run by `github-explorer'."
   :type 'hook
   :group 'github)
 
-(defcustom github-mode-name "GitHub"
-  "*Modeline of `github-mode'."
+(defcustom github-explorer-name "GitHub"
+  "*Modeline of `github-explorer'."
   :type 'string
   :group 'github)
 
-(defvar github-mode-map
+(defvar github-explorer-mode-map
   (let ((keymap (make-sparse-keymap)))
-    (define-key keymap (kbd "o") 'github-go-at-point)
-    (define-key keymap (kbd "RET") 'github-go-at-point)
+    (define-key keymap (kbd "o") 'github-explorer-at-point)
+    (define-key keymap (kbd "RET") 'github-explorer-at-point)
     (define-key keymap (kbd "n") 'next-line)
     (define-key keymap (kbd "p") 'previous-line)
     keymap)
   "Keymap for GitHub major mode.")
 
-(defface github-directory-face
+(defface github-explorer-directory-face
   '((t (:inherit font-lock-function-name-face)))
   "Face for a directory.")
 
-(defvar github-buffer-temp)
+(defvar github-explorer-buffer-temp)
 
-(defvar github-repository)
+(defvar github-explorer-repository)
 
 ;;;###autoload
-(defun github-go (&optional repo)
+(defun github-explorer (&optional repo)
   "Go REPO github."
   (interactive (list (thing-at-point 'symbol)))
-  (setq github-repository (read-string "Repository: " repo))
-  (github--tree (format "https://api.github.com/repos/%s/git/trees/%s" github-repository "master") "/"))
+  (setq github-explorer-repository (read-string "Repository: " repo))
+  (github-explorer--tree (format "https://api.github.com/repos/%s/git/trees/%s" github-explorer-repository "master") "/"))
 
-(defun github-go-at-point()
+(defun github-explorer-at-point()
   "Go to path in buffer GitHub tree."
   (interactive)
   (let (url path (pos 0) matches repo base-path item)
@@ -90,48 +90,48 @@
         (setq path (concat path "/")))
     (message "%s" path)
     (if (string= (cdr (assoc 'type item)) "tree")
-        (github--tree url path)
-      (github--raw repo path))))
+        (github-explorer--tree url path)
+      (github-explorer--raw repo path))))
 
-(defun github--tree (url path)
+(defun github-explorer--tree (url path)
   "Get trees by URL of PATH github.
 This function will create *GitHub:REPO:* buffer"
-  (setq github-buffer-temp (format "*%s:%s:%s*" github-mode-name github-repository path))
+  (setq github-explorer-buffer-temp (format "*%s:%s:%s*" github-explorer-name github-explorer-repository path))
   (request
    url
    :parser 'buffer-string
    :success
    (cl-function (lambda (&key data &allow-other-keys)
                   (when data
-                    (with-current-buffer (get-buffer-create github-buffer-temp)
-                      (let (github-object)
+                    (with-current-buffer (get-buffer-create github-explorer-buffer-temp)
+                      (let (github-explorer-object)
                         (read-only-mode -1)
                         (erase-buffer)
                         (insert data)
                         (switch-to-buffer (current-buffer))
                         (goto-char (point-min))
-                        (setq github-object (json-read))
+                        (setq github-explorer-object (json-read))
                         (erase-buffer)
-                        (insert (format "[*] %s:%s\n" github-repository path))
-                        (github--render-object github-object)
+                        (insert (format "[*] %s:%s\n" github-explorer-repository path))
+                        (github-explorer--render-object github-explorer-object)
                         (goto-char (point-min))
-                        (github-mode))))))
+                        (github-explorer-mode))))))
    :error
    (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
                   (message "Got error: %S" error-thrown)))))
 
-(defun github--raw (repo path)
+(defun github-explorer--raw (repo path)
   "Get raw of PATH in REPO github."
   (let (url)
     (setq url (format "https://raw.githubusercontent.com/%s/master%s" repo path))
-    (setq github-buffer-temp (format "*%s:%s:%s*" github-mode-name repo path))
+    (setq github-explorer-buffer-temp (format "*%s:%s:%s*" github-explorer-name repo path))
     (request
      url
      :parser 'buffer-string
      :success
      (cl-function (lambda (&key data &allow-other-keys)
                     (when data
-                      (with-current-buffer (get-buffer-create github-buffer-temp)
+                      (with-current-buffer (get-buffer-create github-explorer-buffer-temp)
                         (erase-buffer)
                         (insert data)
                         (pop-to-buffer (current-buffer))
@@ -140,46 +140,46 @@ This function will create *GitHub:REPO:* buffer"
      (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
                     (message "Got error: %S" error-thrown))))))
 
-(defun github-apply-auto-mode (&rest _)
+(defun github-explorer-apply-auto-mode (&rest _)
   "Apply auto-mode for buffer GitHub.
 pop-to-buffer(BUFFER-OR-NAME &OPTIONAL ACTION NORECORD)"
   (unless buffer-file-name
-    (if (string-match-p (regexp-quote (format "*%s:" github-mode-name)) (buffer-name))
+    (if (string-match-p (regexp-quote (format "*%s:" github-explorer-name)) (buffer-name))
         (let ((buffer-file-name (substring (buffer-name) 0 -1))) ;; remove * ending
           (set-auto-mode t)))))
-(advice-add 'pop-to-buffer :after #'github-apply-auto-mode)
+(advice-add 'pop-to-buffer :after #'github-explorer-apply-auto-mode)
 
-(defun github--item-path (item)
+(defun github-explorer--item-path (item)
   "Get the path for an ITEM from GitHub."
   (cdr (assoc 'path item)))
 
-(defun github--item-type (item)
+(defun github-explorer--item-type (item)
   "Get the type for an ITEM from GitHub."
   (cdr (assoc 'type item)))
 
-(defun github--render-object (github-object)
-  "Render the GITHUB-OBJECT."
-  (let ((trees (cdr (assoc 'tree github-object))))
+(defun github-explorer--render-object (github-explorer-object)
+  "Render the GITHUB-EXPLORER-OBJECT."
+  (let ((trees (cdr (assoc 'tree github-explorer-object))))
 	(cl-loop
 	 for item across trees
-	 for path = (github--item-path item)
+	 for path = (github-explorer--item-path item)
 
-	 if (string= (github--item-type item) "blob")
+	 if (string= (github-explorer--item-type item) "blob")
 	 do (insert (format "  |----- %s" path))
 	 else do
 	 (insert (format "  |--[+] %s" path))
 	 (left-char (length path))
-	 (put-text-property (point) (+ (length path) (point)) 'face 'github-directory-face)
+	 (put-text-property (point) (+ (length path) (point)) 'face 'github-explorer-directory-face)
 
 	 do
 	 (put-text-property (line-beginning-position) (1+ (line-beginning-position)) 'invisible item)
 	 (end-of-line)
 	 (insert "\n"))))
 
-(define-derived-mode github-mode special-mode github-mode-name
+(define-derived-mode github-explorer-mode special-mode github-explorer-name
   "Major mode for exploring GitHub repository on the fly"
   (setq buffer-auto-save-file-name nil
         buffer-read-only t))
 
-(provide 'github-mode)
-;;; github-mode.el ends here
+(provide 'github-explorer)
+;;; github-explorer.el ends here
